5d656e7e4b79a79cab20bd8568ef2bbbb0165097
Jobar86 <jobar86@hotmail.com>
2017-08-30 18:13:29 -0500

    Initial commit

---
+++ app/src/main/java/com/jbapps/cuentacuentos/favoritos.java
@@ -0,0 +1,20 @@
+package com.jbapps.cuentacuentos;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class favoritos extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favoritos, container, false);
+    }
+}
