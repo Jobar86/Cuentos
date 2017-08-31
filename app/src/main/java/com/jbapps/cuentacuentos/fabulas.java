c19c963ec883dd754ab4ecd8cbab8adfe5baca71
Jobar86 <jobar86@hotmail.com>
2017-08-30 20:06:14 -0500

    diseño del listview

--- app/src/main/java/com/jbapps/cuentacuentos/fabulas.java
+++ app/src/main/java/com/jbapps/cuentacuentos/fabulas.java
@@ -1,5 +1,4 @@
 package com.jbapps.cuentacuentos;
-
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,12 +10,10 @@ import android.view.ViewGroup;
 
 public class fabulas extends Fragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fabulas, container, false);
     }
-
 }
